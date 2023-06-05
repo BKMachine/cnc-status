612dd7c89f5f1a7ddc80c670b38c05b65b6abc7b
DBKynd <dave@dbkynd.com>
2023-06-05 11:30:26 -0600

    move arduino urls to separate module

--- backend/src/machines/Arduino/arduino_polling.ts
+++ backend/src/machines/Arduino/arduino_polling.ts
@@ -1,33 +1,10 @@
 import _axios from 'axios';
 import logger from '../../logger';
-import { arduinoMachines as machines } from '../index';
+import arduinos from './arduino_urls';
 
 const axios = _axios.create({
   timeout: 1000,
 });
-
-const arduinos = [
-  {
-    url: 'http://10.30.1.25:8193',
-    machine: machines.ml1,
-  },
-  {
-    url: 'http://10.30.1.26:8193',
-    machine: machines.ml2,
-  },
-  {
-    url: 'http://10.30.1.27:8193',
-    machine: machines.ml3,
-  },
-  {
-    url: 'http://10.30.1.28:8193',
-    machine: machines.me1,
-  },
-  {
-    url: 'http://10.30.1.29:8193',
-    machine: machines.mz1,
-  },
-];
 
 let interval: NodeJS.Timer;
 
---
+++ backend/src/machines/Arduino/arduino_urls.ts
@@ -0,0 +1,30 @@
+import { arduinoMachines as machines } from '../index';
+
+const arduinos = [
+  {
+    url: 'http://10.30.1.25:8193',
+    machine: machines.ml1,
+  },
+  {
+    url: 'http://10.30.1.26:8193',
+    machine: machines.ml2,
+  },
+  {
+    url: 'http://10.30.1.27:8193',
+    machine: machines.ml3,
+  },
+  {
+    url: 'http://10.30.1.28:8193',
+    machine: machines.me1,
+  },
+  {
+    url: 'http://10.30.1.29:8193',
+    machine: machines.mz1,
+  },
+  {
+    url: 'http://10.30.1.30:8193',
+    machine: machines.mz2,
+  },
+];
+
+export default arduinos;
