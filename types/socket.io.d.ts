9192483df57a59abf981ddacbf43288c74a37784
DBKynd <dave@dbkynd.com>
2024-12-12 12:16:30 -0700

    add event type

--- types/socket.io.d.ts
+++ types/socket.io.d.ts
@@ -2,6 +2,7 @@ interface ServerToClientEvents {
   refresh: () => void;
   change: (status: { id: string; changes: Changes }) => void;
   status: (status: { id: string; status: MachineStatus }) => void;
+  'refresh-data': () => void;
 }
 
-type EmitterEventNames = keyof ServerToClientEvents
+type EmitterEventNames = keyof ServerToClientEvents;
